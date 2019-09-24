2488edb9914a5f78711f83a0d5fad5fd80f40972
NumberI <x0987654321@ya.ru>
2019-09-24 16:30:59 +0300

    choose dollars

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,9 @@ require 'rexml/document'
 class MainController < ApplicationController
   def root
     # http = Net::HTTP.new("http://www.cbr.ru")
-    xml = open("http://www.cbr.ru/scripts/XML_daily.asp?date_req=20/09/2019").read
-    @cur = Hash.from_xml(xml, ['integer'])
+    xml = open("https://www.cbr.ru/scripts/XML_daily.asp?date_req=24/09/2019").read
+    cur = Hash.from_xml(xml, ['integer'])
+    @date = cur["ValCurs"]["Date"]
+    @dollar = cur["ValCurs"]["Valute"].detect{|h| h["Name"] == "Доллар США"}
   end
 end
