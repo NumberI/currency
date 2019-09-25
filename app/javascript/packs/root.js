9c62a5bbaf283820975cc1279f1ffc13a18a3525
NumberI <x0987654321@ya.ru>
2019-09-25 22:16:01 +0300

    remove alert

--- app/javascript/packs/root.js
+++ app/javascript/packs/root.js
@@ -1,1 +1,10 @@
-alert('lo');
+$.ajax({
+    url: '/',
+    dataType: 'json',
+    success: function(data) {
+      console.log(data);
+    },
+    error: function() {
+      alert('error');
+    }
+  });
