f1d2c35fd3765fde2326ff514504551a80d952d0
NumberI <x0987654321@ya.ru>
2019-09-26 23:44:26 +0300

    add annotate and upserting Rate

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,4 +1,18 @@
+# == Schema Information
+#
+# Table name: rates
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  val        :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Rate < ApplicationRecord
+  require 'open-uri'
+  require 'rexml/document'
+
   def self.new_rate
     date = rand(0..20).days.ago.strftime("%d/%m/%Y")
     xml = open("https://www.cbr.ru/scripts/XML_daily.asp?date_req=#{date}").read
@@ -6,5 +20,7 @@ class Rate < ApplicationRecord
     cur = Hash.from_xml(xml, ['integer'])
     @date = cur["ValCurs"]["Date"]
     @dollar = cur["ValCurs"]["Valute"].detect{|h| h["Name"] == "Доллар США"}
+    Rate.upsert(id: 1, name: @dollar["Name"], val: @dollar["Value"], created_at: Time.now, updated_at: Time.now)
+    ActionCable.server.broadcast "exchange_channel", content: @dollar
   end
 end
--- test/models/rate_test.rb
+++ test/models/rate_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: rates
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  val        :float
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class RateTest < ActiveSupport::TestCase
