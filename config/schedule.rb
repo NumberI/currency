99194cb5dc95b688a5334b95eed4c026e38dc914
NumberI <x0987654321@ya.ru>
2019-09-27 23:36:03 +0300

    fix schedule!!
    Yeah!

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,9 @@ set :environment, "development"
 set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 set :job_template, nil
 env :PATH, ENV['PATH']
+set :bundle_command, 'bundle exec'
+job_type :runner,  "cd :path && :bundle_command rails runner -e :environment ':task' :output"
+ENV.each { |k, v| env(k, v) }
 # Use this file to easily define all of your cron jobs.
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
