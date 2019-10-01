b910d7399efb38f690f8d2ba160da3d8e700824f
Аксёнов Александр Сергеевич <aksenov@oscaryard.local>
2019-10-01 21:37:06 +0300

    add datepicker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -5,7 +5,13 @@ module.exports = environment
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
+    // $: 'jquery/src/jquery',
+    // jquery: 'jquery/src/jquery',
+    // jQuery: 'jquery/src/jquery'
+    $: 'jquery-datetimepicker/jquery',
+    jquery: 'jquery-datetimepicker/jquery',
+    jQuery: 'jquery-datetimepicker/jquery',
+    jQuery.datetimepicker: 'jquery-datetimepicker/jquery-datetimepicker'
+
   })
 )
