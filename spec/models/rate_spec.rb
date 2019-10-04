048da5906a9f4cb39daf389ddddd6804a31caed9
NumberI <x0987654321@ya.ru>
2019-10-05 00:29:31 +0300

    add validation of date

--- spec/models/rate_spec.rb
+++ spec/models/rate_spec.rb
@@ -1,5 +1,11 @@
 require "rails_helper"
 
 describe Rate do 
+  
   it {should validate_presence_of :val }
+  
+  it "should be valid" do 
+    Rate.new(val: 123, fixed_till: 2.days.after).should be_valid
+  end
+
 end
