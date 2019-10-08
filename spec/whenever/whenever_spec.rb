a76a089de9046f18cb41b3b68e98cdcbde700ee2
Аксёнов Александр Сергеевич <aksenov@oscaryard.local>
2019-10-08 15:42:04 +0300

    fix whenever test

--- spec/whenever/whenever_spec.rb
+++ spec/whenever/whenever_spec.rb
@@ -1,24 +1,18 @@
 require "rails_helper"
 
 describe 'Whenever Schedule' do
-  before do
-    load 'Rakefile' # Makes sure rake tasks are loaded so you can assert in rake jobs
-  end
+  let(:schedule){ Whenever::Test::Schedule.new(file: 'config/schedule.rb') }
 
   it 'makes sure `runner` statements exist' do
-    schedule = Whenever::Test::Schedule.new(file: 'config/schedule.rb')
-
     assert_equal 1, schedule.jobs[:runner].count
 
     # Executes the actual ruby statement to make sure all constants and methods exist:
     schedule.jobs[:runner].each { |job| instance_eval job[:task] }
   end
 
-  it 'makes sure cron alive monitor is registered in minute basis' do
-    schedule = Whenever::Test::Schedule.new(file: fixture)
-    p schedule.jobs
-    # assert_equal 'http://myapp.com/cron-alive', schedule.jobs[:curl].first[:task]
-    # assert_equal 'curl :task', schedule.jobs[:curl].first[:command]
-    assert_equal [:minute], schedule.jobs[:runner].first[:every]
+  it 'makes sure cron alive monitor is registered in 2 minutes basis' do
+    assert_equal 'Rate.new_rate', schedule.jobs[:runner].first[:task]
+    assert_equal "cd :path && :bundle_command rails runner -e :environment ':task' :output", schedule.jobs[:runner].first[:command]
+    assert_equal "[2 minutes]", schedule.jobs[:runner].first[:every].to_s
   end
 end
